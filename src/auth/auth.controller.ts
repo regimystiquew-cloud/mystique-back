34d3d6ac4fa28486f03b6ddc1a560e1c73992ebd
Ethics03 <honey032004@gmail.com>
2025-11-04 16:36:37 +0530

    feat: added partition for mac

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -34,6 +34,7 @@ export class AuthController {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
         sameSite: 'none',
+        partitioned: true,
         maxAge: 7 * 24 * 60 * 60 * 1000,
       });
       return res.status(HttpStatus.OK).json({
